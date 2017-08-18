7a0c7e78380f0b02d9551eb56a3ad89b8eb92445
bakii_agl <satyam0499@gmail.com>
2017-08-18 15:56:28 +0530

    crowdsale addition

--- contracts/data/DataStore.sol
+++ contracts/data/DataStore.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.11;
 
 contract DataStore {
     address public platform ;
-    // mapping (address => mapping ( address => address) ) public CrowdFundCreators;
+    mapping (address => address ) public CrowdFundCreators;
     mapping (address => address[]) public tokenCreators;
 
     
